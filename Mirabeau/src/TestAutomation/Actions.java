728c6296a9c9d8f8596ae357bf1d6ea4aed93b49
Bogdan Adamutiu <bogdan.adamutiu@gmail.com>
2018-03-21 15:39:25 +0100

    Added method Search

--- Mirabeau/src/TestAutomation/Actions.java
+++ Mirabeau/src/TestAutomation/Actions.java
@@ -7,11 +7,13 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.sikuli.script.Finder;
 import org.sikuli.script.Pattern;
 import org.testng.Reporter;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Actions {
 	
 	FirefoxDriver Mozila = new FirefoxDriver();
+	String NrOfResults;
 	
 	@Test	
 	public void OpenBrowser() {
@@ -26,7 +28,7 @@ public class Actions {
 		}			
 	}
 	
-	@Test (dependsOnMethods = {"OpenBrowser"})
+	@Test (dependsOnMethods = {"OpenBrowser"} , priority = 1)
 	public void VisibleSearchBox() throws InterruptedException {		
 		String ImageLocation = System.getProperty("user.home") + "\\Documents\\Search.PNG";
 		Pattern SearchPicture = new Pattern(ImageLocation);
@@ -42,5 +44,22 @@ public class Actions {
 		}
 	}
 
-	
+	@Test (dependsOnMethods = {"OpenBrowser"} , priority = 2)
+	@Parameters ({"SearchInformation"})
+	public void Search(String SearchInfo) throws InterruptedException {
+		Mozila.findElement(By.xpath("//*[@id=\"search_query_top\"]")).clear();
+		Thread.sleep(500);
+		Mozila.findElement(By.xpath("//*[@id=\"search_query_top\"]")).sendKeys(SearchInfo);
+		Mozila.findElement(By.xpath("/html/body/div/div[1]/header/div[3]/div/div/div[2]/form/button")).click();
+		Thread.sleep(2000);
+		
+		try {
+			Mozila.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div[2]/p"));
+			NrOfResults = Mozila.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div[2]/ul")).getAttribute("childElementCount");
+			Reporter.log("The entered search information has returned "+ NrOfResults +" results");
+		}
+		catch (Exception e) {
+			Reporter.log("No results were found for your search "+ SearchInfo);
+		}
+	}
 }
